8445efa8d316d45073dd5f9932b8d11c5d2ac85f
Shyamanand Yadav <shyamayadav154@gmail.com>
2024-01-10 17:15:24 +0530

    Initial commit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const requestIp = require("request-ip");
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    const clientIp = requestIp.getClientIp(req);
+    res.status(200).json({ name: "John Doe", clientIp: clientIp });
 }
