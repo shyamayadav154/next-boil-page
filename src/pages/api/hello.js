2e81f7e9826aea55fb98def33ae118fe0c3903cd
Shyamanand Yadav <shyamayadav154@gmail.com>
2024-02-22 17:54:46 +0530

    test: ip address

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -3,8 +3,11 @@
 const requestIp = require("request-ip");
 
 export default function handler(req, res) {
-    const clientIp = requestIp.getClientIp(req);
+  const clientIp = requestIp.getClientIp(req);
 
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    res.status(200).json({ name: "John Doe", clientIp: clientIp,ip });
+  const ipv4 = req.connection.remoteAddress; // Get IPv4 address from request object
+  const ipv6 = req.headers["x-forwarded-for"]; // Get IPv6 address from request headers
+
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  res.status(200).json({ name: "John Doe", clientIp: clientIp, ip, ipv4, ipv6});
 }
